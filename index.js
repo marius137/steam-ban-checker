a8d1138ef86897cdf43f275e77eb1c9e8674726a
Marius Kiefer <marius@kekistan.info>
2020-06-30 02:45:26 +0200

    removed emojis because CMD does not support it😥

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const fs = require('fs');
 const readline = require('readline');
 const apiKey = fs.readFileSync('apiKey.txt', 'utf8');
 if (apiKey == "") {
-    console.log("⚠⚠⚠No API Key found, please get one at https://steamcommunity.com/dev/apikey and paste it in apiKey.txt");
+    console.log("No API Key found, please get one at https://steamcommunity.com/dev/apikey and paste it in apiKey.txt");
     process.exit();
 }
 if(apiKey=="0123456789ABCDEF0123456789ABCDEF"){
-    console.log("⚠⚠⚠Stop Trying to use the example API Key! Go and get your own at  https://steamcommunity.com/dev/apikey and paste it in apiKey.txt")
+    console.log("Stop Trying to use the example API Key! Go and get your own at  https://steamcommunity.com/dev/apikey and paste it in apiKey.txt")
     process.exit();
 }
 const steam = new SteamAPI(apiKey);
