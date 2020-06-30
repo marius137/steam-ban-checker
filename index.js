6298563c6e852410cabbc30530b9008bba0a6ca7
Marius Kiefer <marius@kekistan.info>
2020-06-30 02:32:55 +0200

    Reading accounts from accounts.txt

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const SteamAPI = require('steamapi');
 const fs = require('fs');
-var apiKey;
-
-apiKey = fs.readFileSync('apiKey.txt', 'utf8');
-if(apiKey==""){
+const readline = require('readline');
+const apiKey = fs.readFileSync('apiKey.txt', 'utf8');
+if (apiKey == "") {
     console.log("⚠⚠⚠No API Key found, please get one at https://steamcommunity.com/dev/apikey and paste it in apiKey.txt");
     process.exit();
 }
 const steam = new SteamAPI(apiKey);
-steam.resolve('76561198886202103').then(id => {
-    steam.getUserSummary(id).then(summary => {
-        console.log(summary.nickname + " (" + summary.steamID + ")");
-        steam.getUserBans(id).then(PlayerBans => {
-            console.log("VAC Bans: "+PlayerBans.vacBans+" GameBans: "+PlayerBans.gameBans);
+
+
+const rl = readline.createInterface({
+    input: fs.createReadStream('accounts.txt'),
+    output: process.stdout,
+    terminal: false
+});
+
+rl.on('line', (line) => {
+    doBanCheck(line);
+});
+function doBanCheck(steamid) {
+    steam.resolve(steamid).then(id => {
+        var output;
+        steam.getUserSummary(id).then(summary => {
+            output= summary.nickname + " (" + summary.steamID + ")\n"
+            steam.getUserBans(id).then(PlayerBans => {
+                output+="VAC Bans: " + PlayerBans.vacBans + " GameBans: " + PlayerBans.gameBans+"\n";
+                console.log(output);
+            });
         });
     });
-});
+}
