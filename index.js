c74e48fc8742c1c8aff9a6ad66a5ff4e0fa293af
Marius Kiefer <marius@kekistan.info>
2021-09-13 03:00:21 +0200

    accounts are checked in same order as file

--- index.js
+++ index.js
@@ -1,39 +1,34 @@
 const SteamAPI = require('steamapi');
 const fs = require('fs');
-const readline = require('readline');
+
+
 const apiKey = fs.readFileSync('apiKey.txt', 'utf8');
 if (apiKey == "") {
     console.log("No API Key found, please get one at https://steamcommunity.com/dev/apikey and paste it in apiKey.txt");
     process.exit();
 }
-if(apiKey=="0123456789ABCDEF0123456789ABCDEF"){
+if (apiKey == "0123456789ABCDEF0123456789ABCDEF") {
     console.log("Stop Trying to use the example API Key! Go and get your own at  https://steamcommunity.com/dev/apikey and paste it in apiKey.txt")
     process.exit();
 }
+
+
 const steam = new SteamAPI(apiKey);
 
 
-const rl = readline.createInterface({
-    input: fs.createReadStream('accounts.txt'),
-    output: process.stdout,
-    terminal: false
-});
+let array = fs.readFileSync("accounts.txt").toString().split("\n");
+doBanCheck(array);
 
-rl.on('line', (line) => {
-    if (!line.startsWith("#")) {
-        doBanCheck(line);
-    }
-});
-
-function doBanCheck(steamid) {
-    steam.resolve(steamid).then(id => {
-        var output;
-        steam.getUserSummary(id).then(summary => {
+async function doBanCheck(steamid) {
+    for (let i in steamid) {
+        if (!steamid[i].startsWith("#")) {
+            let id=await steam.resolve(steamid[i]);
+            let output;
+            let summary=await steam.getUserSummary(id);
             output = summary.nickname + " (" + summary.steamID + ")\n"
-            steam.getUserBans(id).then(PlayerBans => {
-                output += "VAC Bans: " + PlayerBans.vacBans + " GameBans: " + PlayerBans.gameBans + "\n";
-                console.log(output);
-            });
-        });
-    });
+            let PlayerBans=await steam.getUserBans(id);
+            output += "VAC Bans: " + PlayerBans.vacBans + " GameBans: " + PlayerBans.gameBans + "\n";
+            console.log(output);
+        }
+    }
 }
